7606f6a90fc4da87b807d8eb1d88e31356ff09bd
naymulislam2000 <naim94656@gmail.com>
2026-01-25 12:11:48 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//17: 28
+
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+     <h1 className='text-center'>hello world</h1>
+        
+    </div>
+  )
+}
+
+export default App
