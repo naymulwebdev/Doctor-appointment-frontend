facec1b686cf21869ab236c590b42fe90ed075e2
naymulislam2000 <naim94656@gmail.com>
2026-01-25 16:16:20 +0200

    button components

---
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Testimonial = () => {
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default Testimonial;
